f9d322b91774a829a8e450c6ee517a571440b236
dahye <smallgilr@gmail.com>
2022-04-04 16:12:32 +0900

    media setting

--- albayaPro/calculator/urls.py
+++ albayaPro/calculator/urls.py
@@ -1,8 +1,13 @@
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path
+
+#media
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.calculator, name="calculator"),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- albayaPro/join/urls.py
+++ albayaPro/join/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
+#media
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
     path('', views.join, name="join"),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
