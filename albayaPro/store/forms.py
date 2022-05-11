3867633b3d746e89c0e882536807a849284a11c1
dahye <smallgilr@gmail.com>
2022-05-12 02:59:59 +0900

    create form

--- albayaPro/store/forms.py
+++ albayaPro/store/forms.py
@@ -1,2 +1,24 @@
+from dataclasses import field
+from pyexpat import model
 from django import forms
 from .models import *
+
+class NoticeForm(forms.ModelForm) : 
+    class Meta:
+        model = Notice
+        field = ['title', 'body']
+
+class NoticeCommnetForm(forms.ModelForm) :
+    class Meta:
+        model = NoticeCommnet
+        field = ['comment']
+
+class SuggestionBoxForm(forms.ModelForm) :
+    class Meta:
+        model = SuggestionBox
+        field = ['title', 'body']
+
+class SuggestionCommnetForm(forms.ModelForm) :
+    class Meta:
+        model = SuggestionCommnet
+        field = ['comment']
