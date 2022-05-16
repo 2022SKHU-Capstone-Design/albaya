db6cd3b50a2a478922d8e66418e6fc5fda048249
yoon <cbj0010@naver.com>
2022-05-17 01:49:26 +0900

    read for layout.html develop

--- albayaPro/join/urls.py
+++ albayaPro/join/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.join, name="join"),
+    path('', views.index, name="index"),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
